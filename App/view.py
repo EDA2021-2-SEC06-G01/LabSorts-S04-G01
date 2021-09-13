155640fc1a49b709f3352ccafafc31dbc4400768
Nicolás Palacios <n.palacios2@uniandes.edu.co>
2021-09-13 10:03:18 -0500

    “Entrega Ejemplo – laboratorio 4”

--- App/view.py
+++ App/view.py
@@ -25,7 +25,10 @@ import sys
 import controller
 from DISClib.ADT import list as lt
 assert cf
+import sys
 
+default_limit = 1000
+sys.setrecursionlimit(default_limit*10)
 
 """
 La vista se encarga de la interacción con el usuario
@@ -83,7 +86,15 @@ def printBestBooks(books):
 
 def printSortResults(ord_books, sample=10):
     # TODO completar modificaciones para el laboratorio 4
-    pass
+    size = lt.size(ord_books)
+    if size > sample:
+        print("Los primeros ", sample, " libros ordenados son:")
+        i=1
+        while i <= sample:
+            book = lt.getElement(ord_books,i)
+            print('Titulo: ' + book['title'] + ' ISBN: ' + book['isbn'] + ' Rating: ' + book['average_rating'])
+            i+=1
+    
 
 catalog = None
 
@@ -123,7 +134,8 @@ while True:
         size = input("Indique tamaño de la muestra: ")
         result = controller.sortBooks(catalog, int(size))
         print("Para la muestra de", size, " elementos, el tiempo (mseg) es: ",
-                                          str(result))
+                                          str(result[0]))
+        printSortResults(result[1])
 
     else:
         sys.exit(0)
